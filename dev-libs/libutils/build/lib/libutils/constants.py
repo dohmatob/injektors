1ca642a352977852222fcd49c3b75a810590431e
d0hm4t06 <gmdopp@gmail.com>
2011-12-28 13:08:36 +0100

    .\dev-libs\libutils: fundamental utilities (header files, routine modules, etc.) go here

---
+++ dev-libs/libutils/build/lib/libutils/constants.py
@@ -0,0 +1,23 @@
+"""
+(c) d0hm4t06 3. d0p91m4 (RUDEBOI) December 13, 2011 -BORDEAUX
+"""
+
+from ctypes import c_void_p, c_ulong
+ 
+# Some pretty win32 constant macros
+PAGE_READWRITE = 0x04
+PAGE_EXECUTE_READWRITE = 0x40
+MEM_COMMIT = 0x1000
+MEM_RESERVE = 0x2000
+MEM_RELEASE = 0x8000
+PROCESS_ALL_ACCESS = 0x1F0FFF
+INFINITE = 0xFFFFFFFF
+MB_OK = 0x00
+MB_ICONINFORMATION = 0x40
+MB_ICONERROR = 0x10
+
+# Microsoft types
+HANDLE   = c_void_p
+LPVOID = c_void_p
+DWORD = c_void_p
+LPDWORD = c_ulong
---
+++ dev-libs/libutils/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='libutils',
+    version='0.1dev',
+    packages =[ 'libutils',],
+    license='GPL',
+    long_description=open('README').read(),
+    )
