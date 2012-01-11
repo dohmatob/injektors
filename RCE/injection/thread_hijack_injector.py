c95656e9c8cd9f67142d684938ef5b1945d55221
half-jiffie <gmdopp@gmail.com>
2012-01-11 12:16:59 +0100

    .\RCE\injection\thread_hijack_injector.py: fixed merge conflict with github origin

--- RCE/injection/thread_hijack_injector.py
+++ RCE/injection/thread_hijack_injector.py
@@ -7,7 +7,7 @@ from optparse import OptionParser
 
 kernel32 = windll.kernel32
 
-MAX_DLL_PATHLEN = 100
+MAX_DLL_PATHLEN = 200
 CODECAVE_SIZE = 0x400
 MAX_DLL_FUNCTION_LEN = 25
 
@@ -66,6 +66,103 @@ def hijack(remote_pid,
         sys.exit(1)  
     print "OK."
     print "Building shellcode .."
+    """
+    The following code stub will generate shellcode-code similar to (strings stripped):
+    ..
+     ->| START OF BLOCK (carrier thread's prolog)
+     00260151:           68 44 63 C0 77 PUSH 0x77C06344
+     00260156:                       60 PUSHAD
+     00260157:                       9C PUSHFD
+     ->| START OF BLOCK (load user32.Dll)
+     00260158:           68 32 00 26 00 PUSH 0x260032
+     0026015D:           B8 04 28 0F 76 MOV EAX, 0x760F2804
+     00260162:                    FF D0 CALL EAX
+     |<- END OF BLOCK (load user32.Dll)
+     00260164:           A3 32 00 26 00 MOV DWORD PTR DS:[00260032], EAX
+     ->| START OF BLOCK (import MessageBoxA)
+     00260169:           68 3D 00 26 00 PUSH 0x26003D
+     0026016E:        FF 35 32 00 26 00 PUSH DWORD PTR DS:[0x260032]
+     00260174:           B8 D7 17 0F 76 MOV EAX, 0x760F17D7
+     00260179:                    FF D0 CALL EAX
+     |<- END OF BLOCK (import MessageBoxA)
+     0026017B:           A3 3D 00 26 00 MOV DWORD PTR DS:[0026003D], EAX
+     ->| START OF BLOCK (get inlinedetoursclientdll.dll handle)
+     00260180:           68 E1 00 26 00 PUSH 0x2600E1
+     00260185:           B8 77 28 0F 76 MOV EAX, 0x760F2877
+     0026018A:                    FF D0 CALL EAX
+     |<- END OF BLOCK (get inlinedetoursclientdll.dll handle)
+     0026018C:           3D 00 00 00 00 CMP EAX, 0x0
+     00260191:        0F 84 0A 00 00 00 JZ 0x2601A1
+     00260197:           A3 E1 00 26 00 MOV DWORD PTR DS:[002600E1], EAX
+     0026019C:           E9 1C 00 00 00 JMP 0x2601BD
+     ->| START OF BLOCK (load inlinedetoursclientdll.dll)
+     002601A1:           68 E1 00 26 00 PUSH 0x2600E1
+     002601A6:           B8 04 28 0F 76 MOV EAX, 0x760F2804
+     002601AB:                    FF D0 CALL EAX
+     |<- END OF BLOCK (load inlinedetoursclientdll.dll)
+     002601AD:           3D 00 00 00 00 CMP EAX, 0x0
+     002601B2:        0F 84 30 02 00 00 JZ 0x2603E8
+     002601B8:           A3 E1 00 26 00 MOV DWORD PTR DS:[002600E1], EAX
+     ->| START OF BLOCK (import HookSleepEx)
+     002601BD:           68 49 00 26 00 PUSH 0x260049
+     002601C2:        FF 35 E1 00 26 00 PUSH DWORD PTR DS:[0x2600E1]
+     002601C8:           B8 D7 17 0F 76 MOV EAX, 0x760F17D7
+     002601CD:                    FF D0 CALL EAX
+     |<- END OF BLOCK (import HookSleepEx)
+     002601CF:           A3 49 00 26 00 MOV DWORD PTR DS:[00260049], EAX
+     002601D4:           3D 00 00 00 00 CMP EAX, 0x0
+     002601D9:        0F 84 C3 01 00 00 JZ 0x2603A2
+     ->| START OF BLOCK (invoke HookSleepEx(..))
+     002601DF:           A1 49 00 26 00 MOV EAX, DWORD PTR DS:[0x260049]
+     002601E4:                    FF D0 CALL EAX
+     |<- END OF BLOCK (invoke HookSleepEx(..))
+     ->| START OF BLOCK (NOP-sled)
+     002601E6:                       90 NOP
+     002601E7:                       90 NOP
+     ..
+     0026039B:                       90 NOP
+     0026039C:                       90 NOP
+     |<- END OF BLOCK (NOP-sled)
+     0026039D:           E9 5B 00 00 00 JMP 0x2603FD
+     ->| START OF BLOCK (failed inlinedetoursclientdll.dll ejection notification)
+     002603CE:                    6A 10 PUSH 0x10
+     002603D0:           68 00 00 26 00 PUSH 0x260000
+     002603D5:           68 BA 00 26 00 PUSH 0x2600BA
+     002603DA:                    6A 00 PUSH 0x0
+     002603DC:           A1 3D 00 26 00 MOV EAX, DWORD PTR DS:[0x26003D]
+     002603E1:                    FF D0 CALL EAX
+     |<- END OF BLOCK (failed HookSleepEx(..) from inlinedetoursclientdll.dll notification)
+     002603B7:           E9 41 00 00 00 JMP 0x2603FD
+     ->| START OF BLOCK (unload inlinedetoursclientdll.dll)
+     002603BC:        FF 35 E1 00 26 00 PUSH DWORD PTR DS:[0x2600E1]
+     002603C2:           B8 89 19 0F 76 MOV EAX, 0x760F1989
+     002603C7:                    FF D0 CALL EAX
+     |<- END OF BLOCK (unload inlinedetoursclientdll.dll)
+     002603C9:           E9 2F 00 00 00 JMP 0x2603FD
+     ->| START OF BLOCK (failed inlinedetoursclientdll.dll ejection notification)
+     002603CE:                    6A 10 PUSH 0x10
+     002603D0:           68 00 00 26 00 PUSH 0x260000
+     002603D5:           68 BA 00 26 00 PUSH 0x2600BA
+     002603DA:                    6A 00 PUSH 0x0
+     002603DC:           A1 3D 00 26 00 MOV EAX, DWORD PTR DS:[0x26003D]
+     002603E1:                    FF D0 CALL EAX
+     |<- END OF BLOCK (failed inlinedetoursclientdll.dll ejection notification)
+     002603E3:           E9 15 00 00 00 JMP 0x2603FD
+     ->| START OF BLOCK (failed inlinedetoursclientdll.dll injection notification)
+     002603E8:                    6A 10 PUSH 0x10
+     002603EA:           68 00 00 26 00 PUSH 0x260000
+     002603EF:           68 92 00 26 00 PUSH 0x260092
+     002603F4:                    6A 00 PUSH 0x0
+     002603F6:           A1 3D 00 26 00 MOV EAX, DWORD PTR DS:[0x26003D]
+     002603FB:                    FF D0 CALL EAX
+     |<- END OF BLOCK (failed inlinedetoursclientdll.dll injection notification)
+     ->| START OF BLOCK (carrier thread's epilog)
+     002603FD:                       9D POPFD
+     002603FE:                       61 POPAD
+     002603FF:                       C3 RET
+     |<- END OF BLOCK (carrier thread's epilog)
+    ..
+    """
     shellcode = Shellcode(start_offset=codecave_addr)
     err_caption_addr = shellcode.addConstStr("%s: Error:" %sys.argv[0])
     user32dll_addr = shellcode.addConstStr("user32.dll")
@@ -231,15 +328,17 @@ if __name__ == "__main__":
     options, args = parser.parse_args()
     print __FULL_VERSION__
     if len(args) < 2:
-        debug("Error: Insufficient arguments")
-        die("Use the --help option to get help")
+        print "Error: Insufficient arguments\nUse the --help option to get help"
+        sys.exit(1)
     remote_pid = int(args[0])
     dll_path = os.path.abspath(args[1])
     if len(dll_path) > MAX_DLL_PATHLEN:
-        die("DLL path too long (must be at most %d characters); please rename" %MAX_DLL_PATHLEN)
+        print "DLL path too long (must be at most %d characters); please rename" %MAX_DLL_PATHLEN
+        sys.exit(1)
     if options.function:
         if len(options.function) > MAX_DLL_FUNCTION_LEN:
-            die("function name too long (must be at most %d characters)" %MAX_DLL_FUNCTION_LEN)        
+            print "function name too long (must be at most %d characters)" %MAX_DLL_FUNCTION_LEN
+            sys.exit(1)
     if options.eject:
         if options.function:
             parser.error("It doesn't make any sense to use the '--eject' and '--function' options together; use '--help' option for help")
