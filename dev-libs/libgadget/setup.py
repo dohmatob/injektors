ae3f81cf10093e272529a7a62c1bc1c231953b32
h4lf-jiffie (dohmatob elvis dopgima) <gmdopp@gmail.com>
2012-03-27 00:14:48 +0200

    dev-libs/:  added makefiles in packages

---
+++ dev-libs/libgadget/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='libgadget',
+    version='0.1dev',
+    packages =[ 'libgadget',],
+    license='GPL',
+    long_description=open('README').read(),
+    )
